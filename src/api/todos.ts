672206d9bf6c1d6ee94469dbc96c20470d281f7f
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-03-11 18:03:04 +0200

    initial code

---
+++ src/api/todos.ts
@@ -0,0 +1,28 @@
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const USER_ID = 2305;
+
+export const getTodos = () => {
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const createTodo = ({ userId, title, completed }: Omit<Todo, 'id'>) => {
+  return client.post<Todo>(`/todos`, {
+    userId,
+    title,
+    completed,
+  });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = ({ id, title, completed }: Todo) => {
+  return client.patch<Todo>(`/todos/${id}`, {
+    id,
+    title,
+    completed,
+  });
+};
